0255422732a661554b8a3fd2066069f6bfc3c72c
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-07 15:24:59 +0300

    F - update the "FunWithGenericCollections" project.

--- FunWithGenericCollections/FunWithGenericCollections/Program.cs
+++ FunWithGenericCollections/FunWithGenericCollections/Program.cs
@@ -10,7 +10,8 @@ namespace FunWithGenericCollections
     {
         static void Main(string[] args)
         {
-            UseGenericList();
+            //UseGenericList();
+            UseSortedSet();
         }
 
         //static void UseGenericList()
@@ -41,31 +42,88 @@ namespace FunWithGenericCollections
         //    Console.ReadLine();
         //}
 
+        //static void UseGenericList()
+        //{
+        //    Stack<Person> stackOfPeople = new Stack<Person>();
+        //    stackOfPeople.Push(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+        //    stackOfPeople.Push(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+        //    stackOfPeople.Push(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+        //    Console.WriteLine("First person is: {0}", stackOfPeople.Peek());
+        //    Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+        //    Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
+        //    Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+        //    Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
+        //    Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+        //    try
+        //    {
+        //        Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
+        //        Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+        //    }
+        //    catch (InvalidOperationException ex)
+        //    {
+        //        Console.WriteLine("\nError! {0}", ex.Message);
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
         static void UseGenericList()
         {
-            Stack<Person> stackOfPeople = new Stack<Person>();
-            stackOfPeople.Push(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
-            stackOfPeople.Push(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
-            stackOfPeople.Push(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+            Queue<Person> peopleQ = new Queue<Person>();
+            peopleQ.Enqueue(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            peopleQ.Enqueue(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            peopleQ.Enqueue(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
 
-            Console.WriteLine("First person is: {0}", stackOfPeople.Peek());
-            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
-            Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
-            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
-            Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
-            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            Console.WriteLine("{0} is first at line!", peopleQ.Peek().FirstName);
+
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
+            GetCoffee(peopleQ.Dequeue());
 
             try
             {
-                Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
-                Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+                GetCoffee(peopleQ.Dequeue());
             }
             catch (InvalidOperationException ex)
             {
-                Console.WriteLine("\nError! {0}", ex.Message);
+                Console.WriteLine("Error! {0}", ex.Message);
             }
 
             Console.ReadLine();
+        }
+
+        static void GetCoffee(Person p)
+        {
+            Console.WriteLine("{0} got coffee!", p.FirstName);
+        }
+
+        static void UseSortedSet()
+        {
+            SortedSet<Person> setOfPerson = new SortedSet<Person>(new SortPeopleByAge())
+            {
+                new Person{FirstName = "Homer", LastName = "Simpson", Age = 47},
+                new Person{FirstName = "Marge", LastName = "Simpson", Age = 45},
+                new Person{FirstName = "Lisa", LastName = "Simpson", Age = 9},
+                new Person{FirstName = "Bart", LastName = "Simpson", Age = 8}
+            };
+
+            foreach (Person p in setOfPerson)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.WriteLine();
+
+            setOfPerson.Add(new Person { FirstName = "Saku", LastName = "Jones", Age = 1 });
+            setOfPerson.Add(new Person {FirstName = "Mikko", LastName = "Jones", Age = 32});
+
+            foreach (Person p in setOfPerson)
+            {
+                Console.WriteLine(p);
+            }
+
         }
 
     }
@@ -75,6 +133,19 @@ namespace FunWithGenericCollections
         public int Age { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+    }
+
+    class SortPeopleByAge : IComparer<Person>
+    {
+        public int Compare(Person firstPerson, Person secondPerson)
+        {
+            if (firstPerson.Age > secondPerson.Age)
+                return 1;
+            if (firstPerson.Age < secondPerson.Age)
+                return -1;
+            else
+                return 0;
+        }
     }
 
 }
