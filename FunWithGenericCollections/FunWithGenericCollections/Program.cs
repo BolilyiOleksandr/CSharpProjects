1595a53958f9416087419a8952266a4dbeb6f2d0
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-07 15:34:43 +0300

    F - add the "UseDictionary" method in the "Program.cs" file in the "FunWithGenericCollections" project.

--- FunWithGenericCollections/FunWithGenericCollections/Program.cs
+++ FunWithGenericCollections/FunWithGenericCollections/Program.cs
@@ -11,7 +11,9 @@ namespace FunWithGenericCollections
         static void Main(string[] args)
         {
             //UseGenericList();
-            UseSortedSet();
+            //UseSortedSet();
+
+            UseDictionary();
         }
 
         //static void UseGenericList()
@@ -117,13 +119,25 @@ namespace FunWithGenericCollections
             Console.WriteLine();
 
             setOfPerson.Add(new Person { FirstName = "Saku", LastName = "Jones", Age = 1 });
-            setOfPerson.Add(new Person {FirstName = "Mikko", LastName = "Jones", Age = 32});
+            setOfPerson.Add(new Person { FirstName = "Mikko", LastName = "Jones", Age = 32 });
 
             foreach (Person p in setOfPerson)
             {
                 Console.WriteLine(p);
             }
+        }
 
+        private static void UseDictionary()
+        {
+            Dictionary<string, Person> peopleA = new Dictionary<string, Person>();
+            peopleA.Add("Homer", new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            peopleA.Add("Marge", new Person {FirstName = "Marge", LastName = "Simpson", Age = 45});
+            peopleA.Add("Lisa", new Person {FirstName = "Lisa", LastName = "Simpson", Age = 9});
+
+            Person homer = peopleA["Homer"];
+            Console.WriteLine(homer);
+
+            Console.ReadLine();
         }
 
     }
