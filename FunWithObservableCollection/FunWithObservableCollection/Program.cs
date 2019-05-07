ff34ae51b409368ec1319aae057bc48671f3376d
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-07 15:48:30 +0300

    F - add the "FunWithObservableCollection" project.

---
+++ FunWithObservableCollection/FunWithObservableCollection/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithObservableCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<Person> people = new ObservableCollection<Person>()
+            {
+                new Person {FirstName = "Peter", LastName = "Murphy", Age = 52},
+                new Person {FirstName = "Kevin", LastName = "Key", Age = 48},
+            };
+
+            people.CollectionChanged += People_CollectionChanged;
+
+            Console.ReadLine();
+        }
+
+        private static void People_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Console.WriteLine("Action for this event: {0}", e.Action);
+
+            if (e.Action ==
+                System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("Here are the OLD items:");
+                foreach (Person p in e.OldItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+                Console.WriteLine();
+            }
+            // They added something.
+            if (e.Action ==
+                System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                // Now show the NEW items that were inserted.
+                Console.WriteLine("Here are the NEW items:");
+                foreach (Person p in e.NewItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public enum NotifyCollectionChangedAction
+    {
+        Add = 0,
+        Remove = 1,
+        Replace = 2,
+        Move = 3,
+        Reset = 4,
+    }
+
+}
