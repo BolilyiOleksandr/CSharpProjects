d1502880ea89ef23b50c4289e75487924cb5d057
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-16 16:16:30 +0300

    F - add the "CarDelegate" project.

---
+++ CarDelegate/CarDelegate/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** Delegates as event enablers *****\n");
+
+            var c1 = new Car("SlugBug", 100, 10);
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+
+            Console.WriteLine("***** Speeding up *****");
+            for (var i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine("\n***** Message From Car Object *****");
+            Console.WriteLine("=> {0}", msg);
+            Console.WriteLine("*****************************************************\n");
+        }
+    }
+
+    public class Car
+    {
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        private CarEngineHandler listOfHandlers;
+
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers = methodToCall;
+        }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+        private bool carIsDead;
+        public Car() { }
+
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                    listOfHandlers("Sorry, this car is dead...");
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (10 == (MaxSpeed - CurrentSpeed) && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful buddy! Gonna blow!");
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+
+}
