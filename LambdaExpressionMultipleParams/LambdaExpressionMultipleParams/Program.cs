afca92ac066928284d75b0eaae11422c8eeadd6c
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-22 12:23:33 +0300

    F - add the "LambdaExpressionMultipleParams" project.

---
+++ LambdaExpressionMultipleParams/LambdaExpressionMultipleParams/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionMultipleParams
+{
+    public delegate string VerySimpleDelegate();
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var m = new SimpleMath();
+            m.SetMathHandler((msg, result) => { Console.WriteLine("Message: {0}, Result: {1}", msg, result); });
+            m.Add(10, 10);
+
+            var d = new VerySimpleDelegate(() => { return "Enjoy your string!"; });
+            Console.WriteLine(d());
+
+            Console.ReadLine();
+        }
+    }
+}
