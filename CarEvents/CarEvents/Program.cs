f52efcae68b908d46f3773acf4bcaa1659749590
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-22 12:26:53 +0300

    F - update the "Main" method in the "CarEvents" project.

--- CarEvents/CarEvents/Program.cs
+++ CarEvents/CarEvents/Program.cs
@@ -13,19 +13,22 @@ namespace CarEvents
             Console.WriteLine("***** Fun with Events *****\n");
             var c1 = new Car("SlugBug", 100, 10);
 
-            c1.AboutToBlow += CarIsAlmostDoomed;
-            c1.AboutToBlow += CarAboutToBlow;
+            //c1.AboutToBlow += CarIsAlmostDoomed;
+            //c1.AboutToBlow += CarAboutToBlow;
 
-            EventHandler<CarEventArgs> d = new EventHandler<CarEventArgs>(CarExploded);
-            c1.Exploded += d;
+            //EventHandler<CarEventArgs> d = new EventHandler<CarEventArgs>(CarExploded);
+            //c1.Exploded += d;
 
-            Console.WriteLine("***** Speeding up *****");
-            for (var i = 0; i < 6; i++)
-            {
-                c1.Accelerate(20);
-            }
+            //Console.WriteLine("***** Speeding up *****");
+            //for (var i = 0; i < 6; i++)
+            //{
+            //    c1.Accelerate(20);
+            //}
 
-            c1.Exploded -= d;
+            //c1.Exploded -= d;
+
+            c1.AboutToBlow += (sender, e) => { Console.WriteLine(e.msg); };
+            c1.Exploded += (sender, e) => { Console.WriteLine(e.msg); };
 
             Console.WriteLine("\n***** Speeding up ****");
             for (var i = 0; i < 6; i++)
