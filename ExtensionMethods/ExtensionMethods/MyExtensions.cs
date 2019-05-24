8ea7e4054a0fc3bfb3116f168c093895298a7e2f
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-24 14:55:27 +0300

    F - add the "ExtensionMethods" project.

---
+++ ExtensionMethods/ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ExtensionMethods
+{
+    internal static class MyExtensions
+    {
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine("{0} lives here: => {1}\n", obj.GetType().Name,
+                Assembly.GetAssembly(obj.GetType()).GetName().Name);
+        }
+
+        public static int ReverseDigits(this int i)
+        {
+            var digits = i.ToString().ToCharArray();
+            Array.Reverse(digits);
+            var newDigits = new string(digits);
+            return int.Parse(newDigits);
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Extension Methods *****\n");
+            const int myInt = 12345678;
+            myInt.DisplayDefiningAssembly();
+
+            var d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+
+            var sp = new System.Media.SoundPlayer();
+            sp.DisplayDefiningAssembly();
+
+            Console.WriteLine("Value of myInt: {0}", myInt);
+            Console.WriteLine("Reversed digits of myInt: {0}", myInt.ReverseDigits());
+
+            Console.ReadLine();
+        }
+    }
+}
