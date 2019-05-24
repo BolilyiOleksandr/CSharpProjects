8c98cdd31ab2aacb539827f6824cab0ad04b8212
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-24 16:08:11 +0300

    F - add the "AnonymousTypes" project.

---
+++ AnonymousTypes/AnonymousTypes/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEvents
+{
+    public class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+        private bool carIsDead;
+        public Car() { }
+
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+    }
+}
