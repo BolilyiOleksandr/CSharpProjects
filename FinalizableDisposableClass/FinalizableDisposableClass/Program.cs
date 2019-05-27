74278f7edd3ace80ba1a8e09668859252b3fe3c4
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-27 18:51:26 +0300

    F - add the "FinalizableDisposableClass" project.

---
+++ FinalizableDisposableClass/FinalizableDisposableClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalizableDisposableClass
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** Dispose() / Destructor Combo Platter * ****");
+            // Call Dispose() manually. This will not call the finalizer.
+            var rw = new MyResourceWrapper();
+            rw.Dispose();
+            // Don’t call Dispose(). This will trigger the finalizer
+            // and cause a beep.
+            var rw2 = new MyResourceWrapper();
+        }
+    }
+}
