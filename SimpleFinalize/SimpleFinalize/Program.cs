9125a54c6292daa991a0b60288c315ed3596930c
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-27 17:09:01 +0300

    F - add the "SimpleFinalize" project.

--- SimpleFinalize/SimpleFinalize/Program.cs
+++ SimpleFinalize/SimpleFinalize/Program.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace SimpleFinalize
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Finalizers *****\n");
+            Console.WriteLine("Hit the return key to shut down this app");
+            Console.WriteLine("and force the GC to invoke Finalize()");
+            Console.WriteLine("for finalizable objects created in this AppDomain.");
+            Console.ReadLine();
+
+            var rw = new MyResourceWrapper();
         }
     }
 }
