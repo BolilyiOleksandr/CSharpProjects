ede0f224b1b31785965d0a3ae66b2867611326ab
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-27 18:59:08 +0300

    F - add the "LazyObjectInstantiation" project.

---
+++ LazyObjectInstantiation/LazyObjectInstantiation/AllTracks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyObjectInstantiation
+{
+    internal class AllTracks
+    {
+        // Our media player can have a maximum
+        // of 10,000 songs.
+        private Lazy<AllTracks> allSongs = new Lazy<AllTracks>(
+            () =>
+            {
+                Console.WriteLine("Creating AllTracks object!");
+                return new AllTracks();
+            }
+        );
+        public AllTracks GetAllTracks()
+        {
+            // Return all of the songs.
+            return allSongs.Value;
+        }
+    }
+}
---
+++ LazyObjectInstantiation/LazyObjectInstantiation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyObjectInstantiation
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Lazy Instantiation * ****\n");
+            // No allocation of AllTracks object here!
+            var myPlayer = new MediaPlayer();
+            myPlayer.Play();
+            // Allocation of AllTracks happens when you call GetAllTracks().
+            var yourPlayer = new MediaPlayer();
+            var yourMusic = yourPlayer.GetAllTracks();
+            Console.ReadLine();
+        }
+    }
+}
