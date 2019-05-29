1085a5a49d814dc61a045ac4fa3b8291248c2ddb
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-29 16:46:31 +0300

    F - add the "CarLibrary" project.

---
+++ CarLibrary/CarLibrary/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    public enum EngineState
+    {
+        EngineAlive, EngineDead
+    }
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        protected EngineState EngState = EngineState.EngineAlive;
+
+        public EngineState EngineState
+        {
+            get { return EngState; }
+        }
+
+        public abstract void TurboBoost();
+        public Car() { }
+
+        public Car(string name, int maxSp, int currSp)
+        {
+            PetName = name;
+            MaxSpeed = maxSp;
+            CurrentSpeed = currSp;
+        }
+    }
+}
