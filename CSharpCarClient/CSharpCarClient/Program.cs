ae64a9b8030aef365f778738265bdd96a78acbea
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-29 16:59:51 +0300

    F - add the "CSharpCarClient" project.

---
+++ CSharpCarClient/CSharpCarClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace CSharpCarClient
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** C# CarLibrary Client App *****");
+            var viper = new SportsCar("Viper", 240, 40);
+            viper.TurboBoost();
+
+            var mv = new MiniVan();
+            mv.TurboBoost();
+
+            Console.WriteLine("Done. Press any key to terminate");
+
+            Console.ReadLine();
+        }
+    }
+}
