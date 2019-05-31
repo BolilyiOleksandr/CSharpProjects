2f5260401e6f0453bda1525811592c5fdfadf114
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-05-31 16:15:33 +0300

    F - add the "ApplyingAttributes" project.

---
+++ ApplyingAttributes/ApplyingAttributes/Motorcycle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyingAttributes
+{
+    [Serializable]
+    internal class Motorcycle
+    {
+        [NonSerialized] private float weightOfCurrentPassengers;
+        private bool hasRadioSystem;
+        private bool hasHeadSet;
+        private bool hasSissyBar;
+    }
+}
