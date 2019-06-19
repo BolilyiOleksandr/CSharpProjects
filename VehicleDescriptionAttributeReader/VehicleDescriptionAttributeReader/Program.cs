7871cee9cebf20817f5214e1ae92b89eb4944a63
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-06-19 14:28:19 +0300

    F - add the "VehicleDescriptionAttributeReader" project.

---
+++ VehicleDescriptionAttributeReader/VehicleDescriptionAttributeReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AttributedCarLibrary;
+
+namespace VehicleDescriptionAttributeReader
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** Value of VehicleDescriptionAttribute *****\n");
+            ReflectOnAttributesUsingEarlyBinding();
+
+            Console.ReadLine();
+        }
+
+        private static void ReflectOnAttributesUsingEarlyBinding()
+        {
+            var t = typeof(Winnebago);
+            var customAtts = t.GetCustomAttributes(false);
+
+            foreach (VehicleDescriptionAttribute v in customAtts)
+            {
+                Console.WriteLine("-> {0}\n", v.Description);
+            }
+        }
+
+    }
+
+}
