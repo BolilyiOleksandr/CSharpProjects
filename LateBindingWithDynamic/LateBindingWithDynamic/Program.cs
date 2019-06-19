be177111c2e2b25f2381c0bf43dae405bbbc4f47
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-06-19 17:18:01 +0300

    F - add the "LateBindingWithDynamic" project.

---
+++ LateBindingWithDynamic/LateBindingWithDynamic/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace LateBindingWithDynamic
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            AddWithReflection();
+            AddWithDynamic();
+
+            Console.ReadLine();
+        }
+
+        private static void AddWithReflection()
+        {
+            var asm = Assembly.Load("MathLibrary");
+            try
+            {
+                var math = asm.GetType("MathLibrary.SimpleMath");
+                var obj = Activator.CreateInstance(math);
+                var mi = math.GetMethod("Add");
+                object[] args = {10, 70};
+                Console.WriteLine("Result is: {0}", mi.Invoke(obj, args));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void AddWithDynamic()
+        {
+            var asm = Assembly.Load("MathLibrary");
+            try
+            {
+                var math = asm.GetType("MathLibrary.SimpleMath");
+                dynamic obj = Activator.CreateInstance(math);
+                Console.WriteLine("Result is: {0}", obj.Add(10, 70));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
