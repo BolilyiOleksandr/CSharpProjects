035030a302b638487cfe99dbc1fe48039d168faf
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-06-19 14:42:20 +0300

    F - add the "VehicleAttributeReaderLateBinding" project.

---
+++ VehicleAttributeReaderLateBinding/VehicleAttributeReaderLateBinding/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace VehicleAttributeReaderLateBinding
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** Value of VehicleDescriptionAttribute *****\n");
+            ReflectAttributesUsingLateBinding();
+
+            Console.ReadLine();
+        }
+
+        private static void ReflectAttributesUsingLateBinding()
+        {
+            try
+            {
+                var asm = Assembly.Load("AttributedCarLibrary");
+                var vehicleDesc = asm.GetType("AttributedCarLibrary.VehicleDescriptionAttribute");
+                var propDesc = vehicleDesc.GetProperty("Description");
+                var types = asm.GetTypes();
+
+                foreach (var t in types)
+                {
+                    var objs = t.GetCustomAttributes(vehicleDesc, false);
+
+                    foreach (var o in objs)
+                    {
+                        Console.WriteLine("-> {0}: {1}\n", t.Name, propDesc?.GetValue(o, null));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+
+}
