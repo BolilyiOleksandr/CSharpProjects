ced2b71eb7fbb9cc857df309cfe2b79684200d2c
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-06-25 23:58:54 +0300

    F - add the "AsyncDelegate" project.

---
+++ AsyncDelegate/AsyncDelegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** Async Delegate Invocation *****");
+            Console.WriteLine("Main() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+
+            var b = new BinaryOp(Add);
+            IAsyncResult iftAr = b.BeginInvoke(10, 10, null, null);
+
+            while (!iftAr.IsCompleted)
+            {
+                Console.WriteLine("Doing more work in Main()!");
+                Thread.Sleep(1000);
+            }
+
+            var answer = b.EndInvoke(iftAr);
+            Console.WriteLine("10 + 10 is {0}", answer);
+
+            Console.ReadLine();
+        }
+
+        private static int Add(int x, int y)
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Add() invoked on thread {0}.", Thread.CurrentThread.ManagedThreadId);
+            return x + y;
+        }
+
+    }
+}
