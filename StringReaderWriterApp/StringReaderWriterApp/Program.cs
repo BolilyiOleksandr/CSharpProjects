46b189d55c314ff7c96cc1490325dadea81b1a7c
Oleksandr Bolilyi <Oleksandr_Bolilyi@jabil.com>
2019-07-07 17:56:47 +0300

    F - add the "StringReaderWriterApp" project.

---
+++ StringReaderWriterApp/StringReaderWriterApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StringReaderWriterApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with StringWriter / StringReader *****\n");
+
+            using (var strWriter = new StringWriter())
+            {
+                strWriter.WriteLine("Don't forget Mother's Day this year...");
+                Console.WriteLine("Contents of StringWriter: \n{0}", strWriter);
+
+                var sb = strWriter.GetStringBuilder();
+                sb.Insert(0, "Hey!! ");
+                Console.WriteLine("-> {0}", sb);
+                sb.Remove(0, "Hey!! ".Length);
+                Console.WriteLine("-> {0}", sb);
+            }
+
+            using (var strWriter = new StringWriter())
+            {
+                strWriter.WriteLine("Don't forget Mother's Day this year...");
+                Console.WriteLine("Contents of StringWriter:\n{0}", strWriter);
+
+                using (var strReader = new StringReader(strWriter.ToString()))
+                {
+                    string input = null;
+                    while ((input = strReader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(input);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
