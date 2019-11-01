dde745ee7491264657183eaf5196180b801c4114
Oleksandr Bolilyi <oleksandr.bolilyi@consultant.volvo.com>
2019-11-01 14:11:51 +0100

    F - add the "DataProviderFactory" project.

---
+++ DataProviderFactory/DataProviderFactory/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+
+namespace DataProviderFactory
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Data Provider Factories *****\n");
+
+            var dataProvider = ConfigurationManager.AppSettings["provider"];
+            var connectionString = ConfigurationManager.ConnectionStrings["AutoLotSqlProvider"].ToString();
+            var factory = DbProviderFactories.GetFactory(dataProvider);
+
+            using (var connection = factory.CreateConnection())
+            {
+                if (connection == null)
+                {
+                    ShowError("Connection");
+                    return;
+                }
+
+                Console.WriteLine($"Your connection object is a: {connection.GetType().Name}");
+                connection.ConnectionString = connectionString;
+                connection.Open();
+
+                var command = factory.CreateCommand();
+                if (command == null)
+                {
+                    ShowError("Command");
+                    return;
+                }
+
+                Console.WriteLine($"Your command object is a: {command.GetType().Name}");
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Inventory";
+
+                using (var dataReader = command.ExecuteReader())
+                {
+                    Console.WriteLine($"Your data reader object is a: {dataReader.GetType().Name}");
+                    Console.WriteLine("\n***** Current Inventory *****");
+                    while (dataReader.Read())
+                        Console.WriteLine($"-> Car #{dataReader["CarId"]} is a {dataReader["Make"]}.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void ShowError(string objectName)
+        {
+            Console.WriteLine($"There is an issue creating the {objectName}");
+            Console.ReadLine();
+        }
+    }
+}
