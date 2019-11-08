f845cda9cfbd6ce3bea1ca13ae7ccb5669a4c932
Oleksandr Bolilyi <oleksandr.bolilyi@consultant.volvo.com>
2019-11-08 10:46:20 +0100

    F - add the "InventoryDalDisconnectedGui" project.

---
+++ InventoryDalDisconnectedGui/InventoryDalDisconnectedGui/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace InventoryDalDisconnectedGui
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inventoryGrid = new System.Windows.Forms.DataGridView();
+            this.btnUpdateInventory = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Updated Information";
+            // 
+            // inventoryGrid
+            // 
+            this.inventoryGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inventoryGrid.Location = new System.Drawing.Point(12, 41);
+            this.inventoryGrid.Name = "inventoryGrid";
+            this.inventoryGrid.RowTemplate.Height = 24;
+            this.inventoryGrid.Size = new System.Drawing.Size(776, 323);
+            this.inventoryGrid.TabIndex = 1;
+            // 
+            // btnUpdateInventory
+            // 
+            this.btnUpdateInventory.Location = new System.Drawing.Point(12, 386);
+            this.btnUpdateInventory.Name = "btnUpdateInventory";
+            this.btnUpdateInventory.Size = new System.Drawing.Size(117, 42);
+            this.btnUpdateInventory.TabIndex = 2;
+            this.btnUpdateInventory.Text = "Update!";
+            this.btnUpdateInventory.UseVisualStyleBackColor = true;
+            this.btnUpdateInventory.Click += new System.EventHandler(this.btnUpdateInventory_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdateInventory);
+            this.Controls.Add(this.inventoryGrid);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Simple GUI Front End to the Inventory Table";
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView inventoryGrid;
+        private System.Windows.Forms.Button btnUpdateInventory;
+    }
+}
+
---
+++ InventoryDalDisconnectedGui/InventoryDalDisconnectedGui/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using AutoLotDal.DisconnectedLayer;
+
+namespace InventoryDalDisconnectedGui
+{
+    public partial class MainForm : Form
+    {
+        private InventoryDalDc _dal = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            const string connectionString =
+                @"Data Source=SEGOTW10393726;Initial Catalog=AutoLot;Integrated Security=True;Pooling=False";
+            _dal = new InventoryDalDc(connectionString);
+            inventoryGrid.DataSource = _dal.GetAllInventory();
+        }
+
+        private void btnUpdateInventory_Click(object sender, EventArgs e)
+        {
+            var changedDataTable = (DataTable)inventoryGrid.DataSource;
+            try
+            {
+                _dal.UpdateInventory(changedDataTable);
+                inventoryGrid.DataSource = _dal.GetAllInventory();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+    }
+}
