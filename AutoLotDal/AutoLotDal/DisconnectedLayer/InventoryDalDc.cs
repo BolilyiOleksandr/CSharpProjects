0e00cb35a49408de787ecb448a5a1a1d53c68c5d
Oleksandr Bolilyi <oleksandr.bolilyi@consultant.volvo.com>
2019-11-08 10:35:41 +0100

    F - update the functionality of the "AutoLotDal" project.

---
+++ AutoLotDal/AutoLotDal/DisconnectedLayer/InventoryDalDc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AutoLotDal.DisconnectedLayer
+{
+    public class InventoryDalDc
+    {
+        private readonly string _connectionString;
+        private readonly SqlDataAdapter _adapter = null;
+
+        public InventoryDalDc(string connectionString)
+        {
+            _connectionString = connectionString;
+            ConfigureAdapter(out _adapter);
+        }
+
+        private void ConfigureAdapter(out SqlDataAdapter adapter)
+        {
+            adapter = new SqlDataAdapter("SELECT * FROM Inventory", _connectionString);
+            var builder = new SqlCommandBuilder(adapter);
+        }
+
+        public DataTable GetAllInventory()
+        {
+            var inventory = new DataTable("Inventory");
+            _adapter.Fill(inventory);
+            return inventory;
+        }
+
+        public void UpdateInventory(DataTable modifiedTable)
+        {
+            _adapter.Update(modifiedTable);
+        }
+
+    }
+}
