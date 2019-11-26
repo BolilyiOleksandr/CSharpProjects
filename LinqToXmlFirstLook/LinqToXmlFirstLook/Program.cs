b5a5369b6aaa4a60059775a35e6f7ce9c0ec3fce
Oleksandr Bolilyi <oleksandr.bolilyi@consultant.volvo.com>
2019-11-26 12:14:26 +0100

    F - add the "LinqToXmlFirstLook" project.

---
+++ LinqToXmlFirstLook/LinqToXmlFirstLook/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LinqToXmlFirstLook
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            BuildXmlDocWithDom();
+        }
+
+        private static void BuildXmlDocWithDom()
+        {
+            var doc = new XmlDocument();
+            var inventory = doc.CreateElement("Inventory");
+
+            var car = doc.CreateElement("Car");
+            car.SetAttribute("Id", "1000");
+
+            var name = doc.CreateElement("PetName");
+            name.InnerText = "Jimbo";
+
+            var color = doc.CreateElement("Color");
+            color.InnerText = "Red";
+
+            var make = doc.CreateElement("Make");
+            make.InnerText = "Ford";
+
+            car.AppendChild(name);
+            car.AppendChild(color);
+            car.AppendChild(make);
+            inventory.AppendChild(car);
+            doc.AppendChild(inventory);
+            doc.Save("Inventory.xml");
+        }
+
+    }
+}
