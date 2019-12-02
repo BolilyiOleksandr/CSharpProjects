97fdd03a83c691b062e636406bde55322552b625
Oleksandr Bolilyi <oleksandr.bolilyi@consultant.volvo.com>
2019-12-02 11:30:20 +0100

    F - update the "MagicEightBallServiceClient" project.

--- MagicEightBallServiceClient/MagicEightBallServiceClient/Program.cs
+++ MagicEightBallServiceClient/MagicEightBallServiceClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MagicEightBallServiceClient.ServiceReference1;
 
 namespace MagicEightBallServiceClient
 {
@@ -10,7 +11,17 @@ namespace MagicEightBallServiceClient
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("***** Ask the Magic 8 Ball *****\n");
 
+            using (var ball = new EightBallClient())
+            {
+                Console.Write("Your question: ");
+                var question = Console.ReadLine();
+                var answer = ball.ObtainAnswerToQuestion(question);
+                Console.WriteLine("8-Ball says: {0}", answer);
+            }
+
+            Console.ReadLine();
         }
     }
 }
